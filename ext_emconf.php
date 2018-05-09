12c42e43dd1c36874534a5ff1f4e22e943646725
Oliver Klee <typo3-coding@oliverklee.de>
2018-05-09 17:48:57 +0200

    [TASK] Change the TYPO3 CMS from 7.6-8.7 to 6.2-7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = array(
             'depends' =>
                 array(
                     'php' => '5.6.0-7.2.99',
-                    'typo3' => '7.6.0-8.7.99',
+                    'typo3' => '6.2.30-7.6.99',
                 ),
             'conflicts' =>
                 array(),
