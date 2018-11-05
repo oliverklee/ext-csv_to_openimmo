a80d91a9327de0127ba06e7f624d2feee33b33c7
Oliver Klee <github@oliverklee.de>
2018-11-05 23:47:09 +0100

    [CLEANUP] Streamline ext_emconf.php (#86)

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,22 +2,24 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'CSV-to-OpenImmo converter',
     'description' => 'This extension provides a Scheduler task that reads zipped CSV files from a configured folder and writes zipped OpenImmo files to another configured folder. The task will also copy all image files and PDF from the ZIPs.',
-    'category' => 'module',
+    'version' => '1.0.0',
+    'category' => 'be',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.5.0-7.2.99',
+            'typo3' => '6.2.0-8.7.99',
+            'scheduler' => '6.2.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => false,
+    'createDirs' => 'typo3temp/csv_to_openimmo',
+    'clearCacheOnLoad' => false,
     'author' => 'Oliver Klee',
     'author_email' => 'typo3-coding@oliverklee.de',
     'author_company' => 'oliverklee.de',
-    'state' => 'beta',
-    'createDirs' => 'typo3temp/csv_to_openimmo',
-    'version' => '1.0.0',
-    'constraints' =>
-        [
-            'depends' =>
-                [
-                    'php' => '5.5.0-7.2.99',
-                    'typo3' => '6.2.0-8.7.99',
-                    'scheduler' => '6.2.0-8.7.99',
-                ],
-        ],
     'autoload' => [
         'psr-4' => [
             'OliverKlee\\CsvToOpenImmo\\' => 'Classes/',
