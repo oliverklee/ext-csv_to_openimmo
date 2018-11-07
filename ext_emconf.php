9fa1d3ed705cebfee0496595575ff723788ce310
Oliver Klee <typo3-coding@oliverklee.de>
2018-11-07 09:55:47 +0100

    [TASK] Require TYPO3 >= 7.6
    Also drop some unneeded requirements from the composer.json.

--- ext_emconf.php
+++ ext_emconf.php
@@ -7,8 +7,8 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'php' => '5.5.0-7.2.99',
-            'typo3' => '6.2.0-8.7.99',
-            'scheduler' => '6.2.0-8.7.99',
+            'typo3' => '7.6.0-8.7.99',
+            'scheduler' => '7.6.0-8.7.99',
         ],
         'conflicts' => [],
         'suggests' => [],
